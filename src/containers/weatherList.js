5a9a64242a2d3115fcec8d34b4b71404464f6c3b
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-18 16:38:10 -0700

    [feat]: add chart component to render five day forecast

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from './../components/chart';
+
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const temperature = cityData.list.map(function(weather) {
+      let temp = Number(weather.main.temp);
+      temp = kelvinToFahrenheit(temp);
+      temp = String(temp);
+      return temp;
+    });
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+
+    return (
+      <tr key={ cityData.city.id }>
+        <td>{ cityData.city.name }</td>
+        <td>
+          <Chart data={ temperature } color="orange" units="°F"/>
+        </td>
+        <td>
+          <Chart data={ humidity } color="red" units="%"/>
+        </td>
+        <td>
+          <Chart data={ pressure } color="blue" units="hPa" />
+        </td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (°F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
-
+          { this.props.weather.map(this.renderWeather) }
         </tbody>
       </table> 
     )
   }
 }
 
+function kelvinToFahrenheit(temp) {
+  return Math.floor(temp * 9/5 - 459.67);
+}
+
+// normally state would be our parameter
 function mapStateToProps({ weather }) {
   // coming from the weather key: (in combined reducers)
+  // can replace { weather: state.weather } w/ { weather }
   return { weather }
 }
 
