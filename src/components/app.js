13e563ef4551f2a20c1c0f4e22a9ea8d2e5dd29e
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-20 00:36:17 -0700

    [feat/style]: add and style title

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1>Average five day forecast for your city!</h1>
         <SearchBar />
         <WeatherList />
       </div>
